fb204226e1ea4bbd3343e34a049a93d03e88bc45
oljudi <oljudimark@gmail.com>
2019-07-11 23:37:30 -0500

    correciones de estilos 2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import * as $ from 'jquery';
 import { FormControl, Validators } from '@angular/forms';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { BloggerService } from 'src/app/servicios/blogger.service';
 
 interface MailChimpResponse {
   result: string;
@@ -25,84 +24,8 @@ export class HomeComponent implements OnInit {
   Tipo: string;
   buttons: boolean;
   topbutton: boolean;
-
-  Titulo1: string;
-  Titulo2: string;
-  Titulo3: string;
-  Titulo4: string;
-  parr1: string;
-  parr2: string;
-  parr3: string;
-  img1: string;
-  post: string;
-
-
-  blog: any[] = []; 
-
-  blog2: any[] = []; 
-
-  constructor(
-    protected blogger: BloggerService
-  ) {
-  }
-
-  public ngOnInit() {
-
-  document.querySelectorAll('a[href^="#blog"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-document.querySelectorAll('a[href^="#contacto"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      window.scroll({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    });
-     
-      
-      });
-  });
-  this.blogger.getUsers()
-  .subscribe(
-    (data) => {
-       this.blog = data['results'];
-       console.log(data.items[0].title, data.items[0]); 
-      this.Titulo1 = data.items[0].title;
-      this.img1 = data.items[0].images[0].url;
-      this.Titulo2 = data.items[0].title;
-      this.parr2 = data.items[0].published + " " + data.items[0].url + data.items[0].displayName;
-    
-      this.post = data.items[0].selfLink;
-
-      this.parr1 = this.post+"?callback=handleResponse&key=AIzaSyDaUvpKswFphlKWBAdQOF-AqpCCaaDRSjk";
-
-      
-      console.log(this.post+"?callback=handleResponse&key=AIzaSyDaUvpKswFphlKWBAdQOF-AqpCCaaDRSjk");
-      
-  /*    <div id="content"></div>
-      <script>
-        function handleResponse(response) {
-          document.getElementById("content").innerHTML += "<h1>" + response.title + "</h1>" + response.content;
-        }
-      </script>
-      <script
-      src="https://www.googleapis.com/blogger/v2/blogs/3213900/posts/8398240586497962757?callback=handleResponse&key=YOUR-API-KEY"></script>
-*/
-     },
-  (error) => {
-    console.error(error);
-  }
-  );
  
-
+  public ngOnInit() {
     this.buttons = true;
     this.topbutton = false; 
     $(document).ready(function() {
@@ -129,9 +52,8 @@ document.querySelectorAll('a[href^="#contacto"]').forEach(anchor => {
         }
       });
     });
-
   }
-  
+
   @HostListener('window:scroll', ['$event']) onScrollEvent($event){
     this.buttons = false;
     this.topbutton = true;
